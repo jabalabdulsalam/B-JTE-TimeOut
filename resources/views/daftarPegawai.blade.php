0c3f50d180cbf4b37b579cf8689717e9fad92848
DELL <DELL@DELL-PC>
2018-05-09 20:31:02 +0700

    Daftar Pegawai on Process. Baru selesai halaman

---
+++ resources/views/daftarPegawai.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Daftar Pegawai - TimeOut</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container" style="text-align: center">
+    <h2 style="text-align: center">Daftar Pegawai</h2>
+    <table class="table" style="text-align: center">
+        <thead>
+        <tr>
+            <th style="text-align: center">Nomor</th>
+            <th style="text-align: center">Nama</th>
+            <th style="text-align: center">Jenis Kelamin</th>
+            <th style="text-align: center">Tempat, Tgl Lahir</th>
+            <th style="text-align: center">No.HP</th>
+            <th style="text-align: center">Tahun Masuk</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>Kamal Adhlan Riadi</td>
+            <td>Laki-laki</td>
+            <td>Nigeria, 24 Desember 1997</td>
+            <td>0812-1355-2735</td>
+            <td>2011</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Jabal Abdul Salam</td>
+            <td>Laki-laki</td>
+            <td>Wakanda, 28 Agustus 1999</td>
+            <td>0812-1355-3453</td>
+            <td>2012</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Akmal Fadhilah</td>
+            <td>Laki-laki</td>
+            <td>Meikarta, 17 Agustus 1998</td>
+            <td>0812-1355-3467</td>
+            <td>2012</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Hary Rachmat</td>
+            <td>Laki-laki</td>
+            <td>New York, 14 April 1997</td>
+            <td>0812-1355-5865</td>
+            <td>2013</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>T. Ezandi Trinanda</td>
+            <td>Laki-laki</td>
+            <td>Asgard, 30 September 1997</td>
+            <td>0812-1355-9746</td>
+            <td>2014</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Imadul Auwalin</td>
+            <td>Laki-laki</td>
+            <td>Barcelona, 28 Februari 1997</td>
+            <td>0812-1355-9876</td>
+            <td>2015</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+</body>
+</html>
