c478285a49ed9664fde9bc201a68c4a2e5e8e93c
DELL <DELL@DELL-PC>
2018-05-19 00:01:17 +0700

    Halaman daftarPegawai terhubung

---
+++ resources/views/template/potongan/head.blade.php
@@ -0,0 +1,23 @@
+<!--head-->
+<meta charset="utf-8" />
+<title>TimeOut</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+<meta content="Coderthemes" name="author" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+<!-- App favicon -->
+<link rel="shortcut icon" href="{{ asset("/images/favicon.ico") }}">
+
+<!--Morris Chart CSS -->
+<link rel="stylesheet" href="{{asset("/plugins/morris/morris.css")}}">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+
+<!-- App css -->
+<link href="{{ asset("/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset("/css/icons.css") }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset("/css/metismenu.min.css") }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset("/css/style.css") }}" rel="stylesheet" type="text/css" />
+
+<script src="{{ asset("/js/modernizr.min.js") }}"></script>
+<!-- END head-->
